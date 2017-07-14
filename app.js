96d2d9f28451b83ff2619a1b66ade9a7def4d2f3
mstclair <stclairm@spu.edu>
2017-07-14 16:40:01 -0700

    good number four

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
@@ -127,7 +127,7 @@ function sumArray(testArray){ //eslint-disable-line
 
 }
 
-// TODO: Here is the test for sumArray(); uncomment it to run it
+// DONE: Here is the test for sumArray(); uncomment it to run it
 testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
